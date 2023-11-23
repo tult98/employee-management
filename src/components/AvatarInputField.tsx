8cfc367a011078f35c56ecd6d3d807d625a3dbe5
Tu Thanh Le <tu.le@politetech.com>
2023-11-23 23:51:44 +0700

    fix: incorrect position of camera icon

--- src/components/AvatarInputField.tsx
+++ src/components/AvatarInputField.tsx
@@ -58,7 +58,7 @@ const AvatarInputField = ({ onChange, value, error }: any) => {
         <IconButton
           aria-label='upload picture'
           component='span'
-          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${
+          className={`!absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${
             isHover ? 'opacity-100' : ''
           }`}
         >
